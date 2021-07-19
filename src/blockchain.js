e697150f5aa9f18fc444bc4eb8923e0ae1dcb876
Amol Jagtap <amoljagtap17@gmail.com>
2021-07-19 21:30:07 +0530

    ubdn_prj_1: update requestMessageOwnershipVerification method in blockchain.js

--- src/blockchain.js
+++ src/blockchain.js
@@ -101,7 +101,14 @@ class Blockchain {
    * @param {*} address
    */
   requestMessageOwnershipVerification(address) {
-    return new Promise((resolve) => {})
+    return new Promise((resolve) => {
+      resolve(
+        `${address}:${new Date()
+          .getTime()
+          .toString()
+          .slice(0, -3)}:starRegistry`
+      )
+    })
   }
 
   /**
